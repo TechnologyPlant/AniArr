e030f0ad349fa2e6a7cd48acf195edb90c0050fb
TechnologyPlant <161894783+TechnologyPlant@users.noreply.github.com>
2025-05-29 20:04:37 -0400

    Add docker compose and marten db

--- SyncSenpai.Server/Program.cs
+++ SyncSenpai.Server/Program.cs
@@ -1,4 +1,6 @@
 using SyncSenpai.Server.Components;
+using Marten;
+using Weasel.Core;
 
 namespace SyncSenpai.Server
 {
@@ -7,6 +9,24 @@ namespace SyncSenpai.Server
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // This is the absolute, simplest way to integrate Marten into your
+            // .NET application with Marten's default configuration
+            builder.Services.AddMarten(options =>
+            {
+                // Establish the connection string to your Marten database
+                options.Connection(builder.Configuration.GetConnectionString("Marten")!);
+
+                // Specify that we want to use STJ as our serializer
+                options.UseSystemTextJsonForSerialization();
+
+                // If we're running in development mode, let Marten just take care
+                // of all necessary schema building and patching behind the scenes
+                if (builder.Environment.IsDevelopment())
+                {
+                    options.AutoCreateSchemaObjects = AutoCreate.All;
+                }
+            });
 
             // Add services to the container.
             builder.Services.AddRazorComponents()
