94ac8ea625fbd39e5d8848182276284e26da5cff
TechnologyPlant <161894783+TechnologyPlant@users.noreply.github.com>
2025-07-13 10:14:45 -0400

    Convert to tabbed page

--- syncsenpai.client/src/Pages/Configuration.jsx
+++ syncsenpai.client/src/Pages/Configuration.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import AnilistConfiguration from '../Components/AnilistConfiguration';
 import FribbListConfiguration from '../Components/FribbListConfiguration';
 import SonarrConfiguration from '../Components/SonarrConfiguration';
 
 export default function Configuration() {
+    const [activeTab, setActiveTab] = useState('anilist');
+
     return (
         <div>
-            <AnilistConfiguration />
-            <FribbListConfiguration/>
-            <SonarrConfiguration />
+            <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
+            {activeTab === 'anilist' && <AnilistConfiguration />}
+            {activeTab === 'fribbList' && <FribbListConfiguration />}
+            {activeTab === 'sonarr' && <SonarrConfiguration />}
+        </div>
+    );
+}
+
+function Tabs({ activeTab, setActiveTab }) {
+    return (
+        <div>
+            <button onClick={() => setActiveTab('anilist')} className={activeTab === 'anilist' ? 'active' : ''}>Anilist</button>
+            <button onClick={() => setActiveTab('fribbList')} className={activeTab === 'fribbList' ? 'active' : ''}>FribbList</button>
+            <button onClick={() => setActiveTab('sonarr')} className={activeTab === 'sonarr' ? 'active' : ''}>Sonarr</button>
         </div>
     );
 }
