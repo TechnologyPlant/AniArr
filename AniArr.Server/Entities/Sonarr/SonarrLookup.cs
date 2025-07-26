f1bfe2c0b74e340ec95aa2fc946a400542e86c85
TechnologyPlant <161894783+TechnologyPlant@users.noreply.github.com>
2025-07-26 16:49:27 -0400

    Add base sonarr class and lookup

---
+++ AniArr.Server/Entities/Sonarr/SonarrLookup.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AniArr.Server.Entities.Sonarr;
+
+public class SonarrLookup: SonarrSeriesBase
+{
+    public int year { get; set; }
+    public string path { get; set; }
+    public int id { get; set; }
+    public bool seasonFolder { get; set; }
+    public bool monitored { get; set; }
+    public string folder { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; } = "";
+}
---
+++ AniArr.Server/Entities/Sonarr/SonarrSeriesBase.cs
@@ -0,0 +1,18 @@
+﻿namespace AniArr.Server.Entities.Sonarr;
+
+public abstract class SonarrSeriesBase
+{
+    public int tvdbId { get; set; }
+    public List<Season> seasons { get; set; } = [];
+
+    public class Season
+    {
+        public int seasonNumber { get; set; }
+        public bool monitored { get; set; }
+
+        public override string ToString()
+        {
+            return $"{{\"seasonNumber\":{seasonNumber}, \"monitored\":{monitored} }}";
+        }
+    }
+}
