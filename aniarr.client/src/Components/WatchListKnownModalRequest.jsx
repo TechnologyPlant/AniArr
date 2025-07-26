58b8f3779c0fc503d67e9670a82c38a80d221b20
TechnologyPlant <161894783+TechnologyPlant@users.noreply.github.com>
2025-07-26 17:22:17 -0400

    Add modal for posting request to sonarr (1/2)

---
+++ aniarr.client/src/Components/WatchListKnownModalRequest.jsx
@@ -0,0 +1,171 @@
+import React, { useState, useEffect } from "react";
+import "../App.css";
+const WatchListKnownModalRequest = ({
+  isOpen,
+  onClose,
+  closeModalAndRefresh,
+  watchListItem,
+}) => {
+  if (!isOpen) return null;
+
+  const [lookingUp, setLookingUp] = useState(false);
+  const [lookupDetails, setLookupDetails] = useState({
+    folder: "",
+    monitored: false,
+    seasons: [],
+    title: "",
+    tvdbId: 0,
+    year: 0,
+  });
+
+  useEffect(() => {
+    setLookupDetails(null);
+    fetchAdditionalData();
+  }, []);
+
+  function handleClose() {
+    onClose();
+  }
+  async function handleCloseModalAndRefresh() {
+    await closeModalAndRefresh();
+  }
+  async function acknowledge() {
+    const response = await fetch("WatchListItem", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(watchListItem),
+    });
+    if (response.ok) {
+      alert("Saved watchlist items");
+      await handleCloseModalAndRefresh();
+    } else {
+      alert("Error saving watchlist items");
+    }
+  }
+
+  async function request() {
+    const sonarrConfigResponse = await fetch("Sonarr/config");
+    if (sonarrConfigResponse.ok) {
+      const sonarrConfig = await sonarrConfigResponse.json();
+
+      const sonarrRequest = {
+        QualityProfileId: sonarrConfig.activeQualityProfile.id,
+        RootFolderPath: sonarrConfig.ActiveRootFolder,
+        tvdbId: lookupDetails.tvdbId,
+        seasons: lookupDetails.seasons,
+      };
+
+      const requestResponse = await fetch("Sonarr/Request", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(sonarrRequest),
+      });
+      if (requestResponse.ok) {
+        alert("Saved watchlist items");
+        await acknowledge();
+      } else {
+        alert("Error saving watchlist items");
+      }
+    } else {
+      alert("Error loading sonarr config");
+    }
+  }
+  async function fetchAdditionalData() {
+    try {
+      setLookingUp(true);
+      const response = await fetch(`/Sonarr/Lookup/${watchListItem.title}`);
+      const data = await response.json();
+      setLookupDetails(data);
+      console.log(data);
+    } catch (error) {
+      setLookupDetails(null);
+      console.error("Error fetching additional data:", error);
+    } finally {
+      setLookingUp(false);
+    }
+  }
+
+  function handleSeasonCheckboxChange(seasonNumber) {
+    setLookupDetails((lookupDetail) => ({
+      ...lookupDetail,
+      seasons: lookupDetail.seasons.map((season) =>
+        season.seasonNumber == seasonNumber
+          ? { ...season, monitored: !season.monitored }
+          : season
+      ),
+    }));
+  }
+
+  function handleMonitoredCheckboxChange() {
+    setLookupDetails((lookupDetail) => ({
+      ...lookupDetail,
+      monitored: !lookupDetail.monitored,
+    }));
+  }
+  return (
+    <div
+      className="modal-background"
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        display: isOpen ? "flex" : "none",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        className="modal-panel"
+        style={{ padding: "20px", borderRadius: "8px" }}
+      >
+        <h3>{watchListItem.title}</h3>
+        <button onClick={fetchAdditionalData}>Fetch Data</button>
+
+        {lookupDetails && (
+          <div>
+            <div>
+              <span>Title: {lookupDetails.title}</span>
+            </div>
+            <div>
+              <span>Year: {lookupDetails.year}</span>
+            </div>
+            <div>
+              <span>Monitored? {lookupDetails.monitored}</span>
+              <input
+                type="checkbox"
+                checked={lookupDetails.monitored}
+                onChange={() => handleMonitoredCheckboxChange()}
+              />
+            </div>
+            <div>
+              {lookupDetails.seasons.map((season) => (
+                <div>
+                  <span>Season {season.seasonNumber}</span>
+                  <input
+                    key={season.seasonNumber}
+                    type="checkbox"
+                    checked={season.monitored}
+                    onChange={() =>
+                      handleSeasonCheckboxChange(season.seasonNumber)
+                    }
+                  />
+                </div>
+              ))}
+            </div>
+            <button onClick={acknowledge}>Acknowledge Entry</button>
+            <button onClick={request}>Submit Request</button>
+          </div>
+        )}
+        <button onClick={handleClose}>Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default WatchListKnownModalRequest;
--- aniarr.client/src/Pages/WatchlistUpdate.jsx
+++ aniarr.client/src/Pages/WatchlistUpdate.jsx
@@ -1,22 +1,44 @@
 import { useEffect, useState } from 'react';
 import '../App.css';
-import React from 'react';
+import WatchListKnownModalRequest from '../Components/WatchListKnownModalRequest';
 
+
+// CSS for the grid container
+const gridContainerStyles = {
+    display: 'flex',
+    gap: '20px',
+    flexDirection: 'row',
+    alignItems:'center',
+    textAlign:'left'
+};
 
 export default function WatchlistUpdate() {
 
     const [watchList, setWatchList] = useState([]);
-
-    const LoadAnilistWatchlist = async () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedWatchListItem, setSelectedWatchListItem] = useState('');
+    async function LoadAnilistWatchlist() {
         await fetch('WatchListItem/new')
             .then(res => res.json())
-            .then(data => setWatchList(data))
+            .then(data => setWatchList(data));
     }
 
     useEffect(() => {
         return () => LoadAnilistWatchlist();
     }, []);
+    
+    function openModal(watchListItem) {
+        setSelectedWatchListItem(watchListItem);
+        setIsModalOpen(true);
+    }
 
+    function closeModal() {
+        setIsModalOpen(false);
+    }
+    async function closeModalAndRefresh() {
+        closeModal();
+        await LoadAnilistWatchlist();
+    }
     return (
         <div>
             <h2>Anilist Watchlist</h2>
@@ -25,22 +47,22 @@ export default function WatchlistUpdate() {
                     onClick={() => LoadAnilistWatchlist()}
                 >Refresh</button>
             </div>
-            <div>
+            <div >
                 {watchList?.length > 0 ?
                     (
-                        watchList.map(watchListItem => (
-                            <div>
-                                <span className="border-b py-2">{watchListItem.title}</span>
-
+                         watchList.map(watchListItem => (
+                            <div key={watchListItem.id} style={{ ...gridContainerStyles }} onClick={() => openModal(watchListItem)}>
+                            <span className="border-b py-2">{watchListItem.title}</span>
                                 <ul className="mt-4">
                                     {watchListItem.aniListItems.map(aniListItem => (
-                                        <li className="border-b py-2">{aniListItem.title}</li>
+                                        <li key={aniListItem.id} className="border-b py-2">{aniListItem.title}</li>
                                     ))}
                                 </ul>
                             </div>
                         ))
                     ) : (<p>Loading or no items available.</p>)}
             </div>
+            <WatchListKnownModalRequest isOpen={isModalOpen} onClose={closeModal} closeModalAndRefresh={closeModalAndRefresh} watchListItem={selectedWatchListItem} /> {/* Render the modal */}
         </div>
     );
 }
